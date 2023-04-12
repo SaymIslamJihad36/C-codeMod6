327f07ece7c1d03227faffa3829484cddaa3a227
Saym Islam Jihad <129537314+SaymIslamJihad36@users.noreply.github.com>
2023-04-12 09:42:38 -0700

    Create mod7_5.c

---
+++ mod7_5.c
@@ -0,0 +1,23 @@
+// Module 7.5: Practice Day 02
+
+#include<stdio.h>
+
+int main()
+{
+  long long int n;
+  scanf("%lld",&n);
+  long long int arr[n];
+  long long int sum = 0;
+  for(int i=0;i<n;i++){
+    scanf("%lld",&arr[i]);
+  }
+  for(int i=0;i<n;i++){
+    sum = (sum)+(arr[i]);
+  }
+  if(sum<0){
+    sum=(sum)*(-1);
+    printf("%lld\n",sum);
+  }else{
+    printf("%lld\n",sum);
+  }
+}
