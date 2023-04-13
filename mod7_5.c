e17a7c60a4163627649997a1604a65476800c9be
Saym Islam Jihad <129537314+SaymIslamJihad36@users.noreply.github.com>
2023-04-12 20:41:02 -0700

    Update mod7_5.c

--- mod7_5.c
+++ mod7_5.c
@@ -127,3 +127,23 @@ int main()
   }
   printf("%d %d\n",mn,pos);
 }
+
+// practice problem 6
+
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=n-1;i>=0;i--)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
